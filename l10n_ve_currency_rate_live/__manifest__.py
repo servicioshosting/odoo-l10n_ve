80dda42de968ad85b306cc3173ad4d9fa4e280a1
AstroLui <luisdaniel16an@gmail.com>
2026-07-28 11:46:39 -0400

    [FEAT]: l10n_ve_currency_rate: Merge de los ultimos cambios de Backport

--- l10n_ve_currency_rate_live/__manifest__.py
+++ l10n_ve_currency_rate_live/__manifest__.py
@@ -5,7 +5,7 @@
     "author": "binaural-dev",
     "website": "https://binauraldev.com/",
     "category": "Technical",
-    "version": "17.0.1.1.1",
+    "version": "17.0.1.1.2",
     "depends": ["l10n_ve_rate", "currency_rate_live"],
     "data": [
         "views/res_config_settings.xml",
