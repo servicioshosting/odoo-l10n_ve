6a3b04a46ad810629f38b1c30dbc42b32ba8e9a9
Lich <luisdaniel16an@gmail.com>
2026-08-24 09:49:26 -0400

    [REF] various: merge location_extensions into location

--- l10n_ve_location/models/res_partner.py
+++ l10n_ve_location/models/res_partner.py
@@ -1,15 +1,19 @@
-from odoo import fields, models
+# -*- coding: utf-8 -*-
+import re
+
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError, ValidationError
 
 
 class ResCountryParishBinauralLocalizacion(models.Model):
-    _inherit = "res.partner"
+    _inherit = 'res.partner'
 
     city_id = fields.Many2one("res.country.city", string="City")
 
     city = fields.Char(string="City related",
-                       related="city_id.name", store=True)
+                       related="municipality.name", store=True)
 
-    municipality = fields.Many2one("res.country.municipality", "Municipality")
+    municipality = fields.Many2one("res.country.municipality", "Municipality", domain="[('state_id', '=', state_id)]")
 
     parish_id = fields.Many2one(
         "res.country.parish", domain="[('municipality_id', '=', municipality)]"
